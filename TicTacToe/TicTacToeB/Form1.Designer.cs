1960efcf1b8ba2521bcd048118fc03ceb4075153
NBright24 <78198636+NBright24@users.noreply.github.com>
2021-05-26 19:00:20 +0800

    Redesign
    Design Overhaul. I actually replaced the whole file due to some errors I made that I can't fix. I also made a better design.

---
+++ TicTacToe/TicTacToeB/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿
+namespace TicTacToeB
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.Tile1 = new System.Windows.Forms.Button();
+            this.Tile2 = new System.Windows.Forms.Button();
+            this.Tile3 = new System.Windows.Forms.Button();
+            this.Tile4 = new System.Windows.Forms.Button();
+            this.Tile5 = new System.Windows.Forms.Button();
+            this.Tile6 = new System.Windows.Forms.Button();
+            this.Tile7 = new System.Windows.Forms.Button();
+            this.Tile8 = new System.Windows.Forms.Button();
+            this.Tile9 = new System.Windows.Forms.Button();
+            this.RestartBTN = new System.Windows.Forms.Button();
+            this.HelpMe = new System.Windows.Forms.HelpProvider();
+            this.TurnLabel = new System.Windows.Forms.Label();
+            this.ScoreLabel = new System.Windows.Forms.Label();
+            this.OSccore = new System.Windows.Forms.Label();
+            this.XScore = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TurnDisplay = new System.Windows.Forms.Label();
+            this.DisplayXScore = new System.Windows.Forms.Label();
+            this.DisplayOScore = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Tile1
+            // 
+            this.Tile1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Tile1.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Tile1.Location = new System.Drawing.Point(12, 12);
+            this.Tile1.Name = "Tile1";
+            this.Tile1.Size = new System.Drawing.Size(75, 75);
+            this.Tile1.TabIndex = 0;
+            this.Tile1.UseVisualStyleBackColor = false;
+            this.Tile1.Click += new System.EventHandler(this.Tile1_Click);
+            // 
+            // Tile2
+            // 
+            this.Tile2.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Tile2.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Tile2.Location = new System.Drawing.Point(93, 12);
+            this.Tile2.Name = "Tile2";
+            this.Tile2.Size = new System.Drawing.Size(75, 75);
+            this.Tile2.TabIndex = 1;
+            this.Tile2.UseVisualStyleBackColor = false;
+            // 
+            // Tile3
+            // 
+            this.Tile3.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Tile3.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Tile3.Location = new System.Drawing.Point(174, 12);
+            this.Tile3.Name = "Tile3";
+            this.Tile3.Size = new System.Drawing.Size(75, 75);
+            this.Tile3.TabIndex = 2;
+            this.Tile3.UseVisualStyleBackColor = false;
+            // 
+            // Tile4
+            // 
+            this.Tile4.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Tile4.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Tile4.Location = new System.Drawing.Point(12, 93);
+            this.Tile4.Name = "Tile4";
+            this.Tile4.Size = new System.Drawing.Size(75, 75);
+            this.Tile4.TabIndex = 3;
+            this.Tile4.UseVisualStyleBackColor = false;
+            // 
+            // Tile5
+            // 
+            this.Tile5.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Tile5.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Tile5.Location = new System.Drawing.Point(93, 93);
+            this.Tile5.Name = "Tile5";
+            this.Tile5.Size = new System.Drawing.Size(75, 75);
+            this.Tile5.TabIndex = 4;
+            this.Tile5.UseVisualStyleBackColor = false;
+            // 
+            // Tile6
+            // 
+            this.Tile6.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Tile6.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Tile6.Location = new System.Drawing.Point(174, 93);
+            this.Tile6.Name = "Tile6";
+            this.Tile6.Size = new System.Drawing.Size(75, 75);
+            this.Tile6.TabIndex = 5;
+            this.Tile6.UseVisualStyleBackColor = false;
+            // 
+            // Tile7
+            // 
+            this.Tile7.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Tile7.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Tile7.Location = new System.Drawing.Point(12, 174);
+            this.Tile7.Name = "Tile7";
+            this.Tile7.Size = new System.Drawing.Size(75, 75);
+            this.Tile7.TabIndex = 6;
+            this.Tile7.UseVisualStyleBackColor = false;
+            // 
+            // Tile8
+            // 
+            this.Tile8.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Tile8.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Tile8.Location = new System.Drawing.Point(93, 174);
+            this.Tile8.Name = "Tile8";
+            this.Tile8.Size = new System.Drawing.Size(75, 75);
+            this.Tile8.TabIndex = 7;
+            this.Tile8.UseVisualStyleBackColor = false;
+            // 
+            // Tile9
+            // 
+            this.Tile9.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.Tile9.Font = new System.Drawing.Font("Comic Sans MS", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Tile9.Location = new System.Drawing.Point(174, 174);
+            this.Tile9.Name = "Tile9";
+            this.Tile9.Size = new System.Drawing.Size(75, 75);
+            this.Tile9.TabIndex = 8;
+            this.Tile9.UseVisualStyleBackColor = false;
+            // 
+            // RestartBTN
+            // 
+            this.RestartBTN.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.RestartBTN.Font = new System.Drawing.Font("Comic Sans MS", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.RestartBTN.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.RestartBTN.Location = new System.Drawing.Point(93, 416);
+            this.RestartBTN.Name = "RestartBTN";
+            this.RestartBTN.Size = new System.Drawing.Size(75, 34);
+            this.RestartBTN.TabIndex = 9;
+            this.RestartBTN.Text = " RESTART";
+            this.RestartBTN.UseVisualStyleBackColor = false;
+            // 
+            // TurnLabel
+            // 
+            this.TurnLabel.AutoSize = true;
+            this.TurnLabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TurnLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TurnLabel.Location = new System.Drawing.Point(81, 265);
+            this.TurnLabel.Name = "TurnLabel";
+            this.TurnLabel.Size = new System.Drawing.Size(101, 21);
+            this.TurnLabel.TabIndex = 10;
+            this.TurnLabel.Text = "Whose Turn?";
+            // 
+            // ScoreLabel
+            // 
+            this.ScoreLabel.AutoSize = true;
+            this.ScoreLabel.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ScoreLabel.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ScoreLabel.Location = new System.Drawing.Point(12, 377);
+            this.ScoreLabel.Name = "ScoreLabel";
+            this.ScoreLabel.Size = new System.Drawing.Size(90, 21);
+            this.ScoreLabel.TabIndex = 11;
+            this.ScoreLabel.Text = "Scoreboard";
+            // 
+            // OSccore
+            // 
+            this.OSccore.AutoSize = true;
+            this.OSccore.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.OSccore.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.OSccore.Location = new System.Drawing.Point(199, 351);
+            this.OSccore.Name = "OSccore";
+            this.OSccore.Size = new System.Drawing.Size(19, 21);
+            this.OSccore.TabIndex = 12;
+            this.OSccore.Text = "0";
+            // 
+            // XScore
+            // 
+            this.XScore.AutoSize = true;
+            this.XScore.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.XScore.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.XScore.Location = new System.Drawing.Point(123, 351);
+            this.XScore.Name = "XScore";
+            this.XScore.Size = new System.Drawing.Size(19, 21);
+            this.XScore.TabIndex = 13;
+            this.XScore.Text = "X";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(81, 297);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 21);
+            this.label1.TabIndex = 14;
+            // 
+            // TurnDisplay
+            // 
+            this.TurnDisplay.AutoSize = true;
+            this.TurnDisplay.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.TurnDisplay.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.TurnDisplay.Location = new System.Drawing.Point(80, 297);
+            this.TurnDisplay.Name = "TurnDisplay";
+            this.TurnDisplay.Size = new System.Drawing.Size(102, 21);
+            this.TurnDisplay.TabIndex = 15;
+            this.TurnDisplay.Text = "                       ";
+            this.TurnDisplay.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // DisplayXScore
+            // 
+            this.DisplayXScore.AutoSize = true;
+            this.DisplayXScore.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.DisplayXScore.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DisplayXScore.Location = new System.Drawing.Point(123, 377);
+            this.DisplayXScore.Name = "DisplayXScore";
+            this.DisplayXScore.Size = new System.Drawing.Size(19, 21);
+            this.DisplayXScore.TabIndex = 16;
+            this.DisplayXScore.Text = "0";
+            // 
+            // DisplayOScore
+            // 
+            this.DisplayOScore.AutoSize = true;
+            this.DisplayOScore.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.DisplayOScore.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.DisplayOScore.Location = new System.Drawing.Point(199, 377);
+            this.DisplayOScore.Name = "DisplayOScore";
+            this.DisplayOScore.Size = new System.Drawing.Size(19, 21);
+            this.DisplayOScore.TabIndex = 17;
+            this.DisplayOScore.Text = "0";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(261, 462);
+            this.Controls.Add(this.DisplayOScore);
+            this.Controls.Add(this.DisplayXScore);
+            this.Controls.Add(this.TurnDisplay);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.XScore);
+            this.Controls.Add(this.OSccore);
+            this.Controls.Add(this.ScoreLabel);
+            this.Controls.Add(this.TurnLabel);
+            this.Controls.Add(this.RestartBTN);
+            this.Controls.Add(this.Tile9);
+            this.Controls.Add(this.Tile8);
+            this.Controls.Add(this.Tile7);
+            this.Controls.Add(this.Tile6);
+            this.Controls.Add(this.Tile5);
+            this.Controls.Add(this.Tile4);
+            this.Controls.Add(this.Tile3);
+            this.Controls.Add(this.Tile2);
+            this.Controls.Add(this.Tile1);
+            this.HelpButton = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "TicTacToe";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Tile1;
+        private System.Windows.Forms.Button Tile2;
+        private System.Windows.Forms.Button Tile3;
+        private System.Windows.Forms.Button Tile4;
+        private System.Windows.Forms.Button Tile5;
+        private System.Windows.Forms.Button Tile6;
+        private System.Windows.Forms.Button Tile7;
+        private System.Windows.Forms.Button Tile8;
+        private System.Windows.Forms.Button Tile9;
+        private System.Windows.Forms.Button RestartBTN;
+        private System.Windows.Forms.HelpProvider HelpMe;
+        private System.Windows.Forms.Label TurnLabel;
+        private System.Windows.Forms.Label ScoreLabel;
+        private System.Windows.Forms.Label OSccore;
+        private System.Windows.Forms.Label XScore;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label TurnDisplay;
+        private System.Windows.Forms.Label DisplayXScore;
+        private System.Windows.Forms.Label DisplayOScore;
+    }
+}
+
